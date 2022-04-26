11ff75a727d7ddc25dce54bf68503c1cab0e9144
billy mcdermott <billy@billymcdermott.com>
2022-04-26 18:05:17 -0400

    updated dependabot dependencies, updated reidirect to billy.xxx

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return redirect("http://billymcdermott.xxx", code=302)
+	return redirect("https://billy.xxx", code=302)
 
 if __name__=="__main__":
 	app.run()
